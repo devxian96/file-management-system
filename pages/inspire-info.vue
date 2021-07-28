c1db67482d9ab188cde3242781f083bddeb61fc1
sunwongKim <face712@naver.com>
2021-07-29 00:46:32 +0900

    fix: ver2 - 카드 섹션 디자인

--- pages/inspire-info.vue
+++ pages/inspire-info.vue
@@ -1,193 +1,154 @@
 <template>
   <div>
-    <!-- 회원 정보 수정 시작 -->
-    <!-- 오른쪽 위 동그란 버튼 눌러야 수정 후 저장 가능 -->
-    <v-card
-      class="overflow-hidden"
-      color="purple lighten-1"
-      dark
-    >
-      <v-toolbar
-        flat
-        color="purple"
-      >
-        <v-icon>mdi-account</v-icon>
-        <v-toolbar-title class="font-weight-light">
-          User Profile
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn
-          color="purple darken-3"
-          fab
-          small
-          @click="isEditing = !isEditing"
-        >
-          <v-icon v-if="isEditing">
-            mdi-close
-          </v-icon>
-          <v-icon v-else>
-            mdi-pencil
-          </v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-card-text>
-      <v-row>
-        <v-col cols="12" sm="6">
-          <v-text-field
-            :disabled="!isEditing"
-            color="white"
-            label="ID"
-          ></v-text-field>
-          </v-col>
-        <v-col cols="12" sm="6">
-          <v-text-field label="비밀번호" filled />
-        </v-col>
-
-        <v-col cols="12" sm="6">
-          <v-text-field label="이름" filled />
-        </v-col>
-
-        <v-col cols="12" sm="6">
-          <v-text-field label="전화번호" filled />
-        </v-col>
-
-        <v-col cols="12" sm="6">
-          <v-text-field label="부서" filled />
-        </v-col>
-
-        <v-col cols="12" sm="6">
-          <v-text-field label="직책" filled />
-        </v-col>
-
-        <v-col cols="12" sm="6">
-          <v-text-field label="IP" filled />
-        </v-col>
-
-        <v-col cols="12" sm="6">
-          <v-autocomplete
-            :disabled="!isEditing"
-            :items="states"
-            :filter="customFilter"
-            color="white"
-            item-text="name"
-            label="권한"
-          ></v-autocomplete>
-          </v-col>
-
-        <v-col>
-          <v-text-field label="설명" filled />
-        </v-col>
-        </v-row>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          :disabled="!isEditing"
+    <v-row>
+      <!-- 회원 정보 수정 시작 -->
+      <!-- 오른쪽 위 동그란 버튼 눌러야 수정 후 저장 가능 -->
+      <v-col>
+        <v-card class="overflow-hidden">
+          <v-toolbar 
+          flat
           color="success"
-          @click="save"
-        >
-          Save
-        </v-btn>
-      </v-card-actions>
-      <v-snackbar
-        v-model="hasSaved"
-        :timeout="2000"
-        absolute
-        bottom
-        left
-      >
-        Your profile has been updated
-      </v-snackbar>
-    </v-card>
+          dark
+          >
+            <v-icon>mdi-account</v-icon>
+            <v-toolbar-title class="font-weight-medium">
+              User Profile
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="light-green darken-4"
+              fab
+              small
+              @click="isEditing = !isEditing"
+            >
+              <v-icon v-if="isEditing">
+                mdi-close
+              </v-icon>
+              <v-icon v-else>
+                mdi-pencil
+              </v-icon>
+            </v-btn>
+          </v-toolbar>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" sm="6">
+                <v-text-field :disabled="!isEditing" label="ID" filled dense/>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <v-text-field :disabled="!isEditing" label="비밀번호" filled dense/>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <v-text-field :disabled="!isEditing" label="이름" filled dense/>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <v-text-field :disabled="!isEditing" label="전화번호" filled dense/>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <v-text-field :disabled="!isEditing" label="부서" filled dense/>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <v-text-field :disabled="!isEditing" label="직책" filled dense/>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <v-text-field :disabled="!isEditing" label="IP" filled dense/>
+              </v-col>
+
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :disabled="!isEditing" 
+                  :items="states"
+                  :filter="customFilter"
+                  item-text="name"
+                  label="권한"
+                  filled
+                  dense
+                ></v-autocomplete>
+                </v-col>
+
+              <v-col>
+                <v-text-field :disabled="!isEditing" label="설명" filled dense/>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              :disabled="!isEditing"
+              color="success"
+              @click="save"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+          <v-snackbar
+            v-model="hasSaved"
+            :timeout="2000"
+            absolute
+            bottom
+            left
+          >
+            Your profile has been updated
+          </v-snackbar>
+        </v-card>
+      </v-col>
+      <!-- 회원 정보 수정 종료 -->
+
+      <!-- 최근 수정한 문서 시작 -->
+      <v-col class="document" cols="4">
+        <v-card class="mb-5">
+          <v-card-title>최근 수정한 문서</v-card-title>
+          <v-card-text>
+            <v-list>
+              <v-list-item nuxt link dense>
+                <v-list-item-title>문서이름</v-list-item-title>
+                <v-list-item-subtitle>{{
+                  new Date().toLocaleString()
+                }}</v-list-item-subtitle>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
+        <!-- 최근 수정한 문서 종료 -->
+
+        <!-- 최근 확인한 문서 시작 -->
+        <v-card>
+          <v-card-title>최근 확인한 문서</v-card-title>
+          <v-card-text>
+            <v-list>
+              <v-list-item nuxt link dense>
+                <v-list-item-title>문서이름</v-list-item-title>
+                <v-list-item-subtitle>{{
+                  new Date().toLocaleString()
+                }}</v-list-item-subtitle>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
+        <!-- 최근 확인한 문서 종료 -->
+      </v-col>
+    </v-row>
     <!-- 회원 정보 수정 종료 -->
 
     <!-- 접속 기록 시작 -->
-    <v-data-table
-      dense
-      :headers="headers"
-      :items="messages"
-      item-key="from"
-      class="elevation-1"
-    ></v-data-table>
+    <v-card class="log">
+      <v-card-title>
+        접속 기록
+      </v-card-title>
+      <v-data-table
+        dense
+        :headers="headers"
+        :items="messages"
+        item-key="from"
+        class="elevation-1"
+      ></v-data-table>
+    </v-card>
     <!-- 접속 기록 종료 -->
-
-    <div>
-      <v-divider class="mt-5 mb-5" />
-
-      <v-row>
-        <!-- 로그인 이력 시작 -->
-        <v-col cols="4">
-          <v-card>
-            <v-img height="200px" src="https://picsum.photos/200/300">
-              <v-app-bar flat color="transparent"></v-app-bar>
-
-              <v-card-title class="white--text mt-8">
-                <v-avatar color="success">
-                  <v-icon dark> fa-user </v-icon>
-                </v-avatar>
-                <p class="ml-3 mt-3">{{ name }}님 환영합니다</p>
-              </v-card-title>
-            </v-img>
-
-            <v-card-text>
-              <div class="font-weight-bold ml-8 mb-2">금일 로그인 이력</div>
-
-              <v-timeline align-top dense>
-                <v-timeline-item
-                  v-for="message in messages"
-                  :key="message.time"
-                  :color="message.color"
-                  small
-                >
-                  <div>
-                    <div class="font-weight-normal">
-                      <strong>{{ message.from }}</strong> @{{ message.time }}
-                    </div>
-                    <div>{{ message.message }}</div>
-                  </div>
-                </v-timeline-item>
-              </v-timeline>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <!-- 로그인 이력 종료 -->
-
-        <v-col>
-          <!-- 최근 수정한 문서 시작 -->
-          <v-card class="mb-5">
-            <v-card-title>최근 수정한 문서</v-card-title>
-            <v-card-text>
-              <v-list>
-                <v-list-item nuxt link dense>
-                  <v-list-item-title>문서이름</v-list-item-title>
-                  <v-list-item-subtitle>{{
-                    new Date().toLocaleString()
-                  }}</v-list-item-subtitle>
-                </v-list-item>
-              </v-list>
-            </v-card-text>
-          </v-card>
-          <!-- 최근 수정한 문서 종료 -->
-
-          <!-- 최근 확인한 문서 시작 -->
-          <v-card>
-            <v-card-title>최근 확인한 문서</v-card-title>
-            <v-card-text>
-              <v-list>
-                <v-list-item nuxt link dense>
-                  <v-list-item-title>문서이름</v-list-item-title>
-                  <v-list-item-subtitle>{{
-                    new Date().toLocaleString()
-                  }}</v-list-item-subtitle>
-                </v-list-item>
-              </v-list>
-            </v-card-text>
-          </v-card>
-          <!-- 최근 확인한 문서 종료 -->
-        </v-col>
-      </v-row>
-    </div>
 </div>
 </template>
 
@@ -196,16 +157,14 @@
     data () {
       return {
         hasSaved: false,
-        isEditing: null,
+        isEditing: false,
         model: null,
         states: [
-          { name: 'Florida', abbr: 'FL', id: 1 },
-          { name: 'Georgia', abbr: 'GA', id: 2 },
-          { name: 'Nebraska', abbr: 'NE', id: 3 },
-          { name: 'California', abbr: 'CA', id: 4 },
-          { name: 'New York', abbr: 'NY', id: 5 },
+          { name: '1', abbr: 'FL', id: 1 },
+          { name: '2', abbr: 'GA', id: 2 },
+          { name: '3', abbr: 'NE', id: 3 },
+          { name: '4', abbr: 'CA', id: 4 },
         ],
-        name: '관리자',
         headers: [
           {
             align: 'start',
@@ -280,3 +239,16 @@
     },
   }
 </script>
+
+<style>
+  .document {
+    display: flex;
+    flex-direction: column;
+  }
+  .document * {
+    flex-grow: 1;
+  }
+  .log {
+    margin-top: 20px;
+  }
+</style>
